68ea0398b63b64d7db0655ea057fa503a256d022
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-01-06 18:07:51 +0700

    updated script

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function isSolved(board) {
+let thisPlay = 1;
+board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+
+function checkWin(board) {
     let [row1, row2, row3] = board;
     let result = 0;
     let isFinish = false;
+
 
 
     // Gorizontal Check
@@ -12,13 +20,10 @@ function isSolved(board) {
         }
     });
 
-
     // Check
     if (isFinish == true) {
         return result;
     }
-
-
 
     // Vertical Check
     for (let i = 0; i != 3; i++) {
@@ -30,15 +35,13 @@ function isSolved(board) {
 
     // Corner Check
     if (row1[0] == row2[1] && row1[0] == row3[2] && row1[0] != 0) {
+        result = row1[0];
+        return result;
+    }
+    if (row3[0] == row2[1] && row1[2] == row3[0] && row1[2] != 0) {
         result = row1[0]
         return result;
     }
-    if (row3[0] == row2[1] && row1[2] == row3[2] && row1[3] != 0) {
-        result = row1[0]
-        return result;
-    }
-
-
 
     // Check to Empty
     row1.forEach((el) => {
@@ -57,14 +60,50 @@ function isSolved(board) {
         }
     });
     if (isFinish == true) {
-        return -1;
+        return 0;
     }
 
-    return 0;
+    return -1;
 }
 
-console.log(isSolved([
-    [0, 2, 2],
-    [2, 1, 1],
-    [0, 0, 1],
-]));
+function renderBoard(x, y) {
+    let cell = document.querySelector(`.cell${x}${y}`)
+
+    if (thisPlay == 1) {
+        cell.innerHTML = 'X';
+    }
+    if (thisPlay == 2) {
+        cell.innerHTML = 'O';
+    }
+}
+
+function gameClick(x, y) {
+    if (board[x][y] == 0) {
+        if (thisPlay == 1) {
+            board[x][y] = thisPlay;
+            renderBoard(x, y);
+            thisPlay = 2;
+        } else if (thisPlay == 2) {
+            board[x][y] = thisPlay;
+            renderBoard(x, y);
+            thisPlay = 1;
+        }
+    }
+
+    let winner = checkWin(board);
+    ifWin(winner);
+}
+
+function ifWin(winner) {
+    switch (winner) {
+        case 1:
+            console.log('x won');
+            break;
+        case 2:
+            console.log('y won');
+            break;
+        case -1:
+            console.log('draw');
+            break;
+    }
+}
